caa2b56153a14c53464f906868acd54216148bf3
lxp <hello_xp@outlook.com>
2022-10-31 17:11:02 +0800

    样式

--- src/charts/must-get-chart.tsx
+++ src/charts/must-get-chart.tsx
@@ -12,15 +12,6 @@ export function MustGetChart(props: MustGetChartProps) {
     const {wish, simulateTimes = 20000} = props;
     let arr: number[] = [];
     let wishes: MustGetWish[] = Array.isArray(wish) ? wish : [wish];
-    const matchTargets = (bingo: Map<[number, string], number>, targets: Map<[number, string], number>) => {
-        let flag: boolean = true;
-        targets.forEach((value, key) => {
-            if ((bingo.get(key) as number) < value) {
-                flag = false;
-            }
-        });
-        return flag;
-    };
     for (let i = 0; i < simulateTimes; i++) {
         let total = 0;
         for (let j = 0; j < wishes.length; j++) {
@@ -37,7 +28,6 @@ export function MustGetChart(props: MustGetChartProps) {
             targets.forEach((value: number, key: [number, string]) => {
                 bingo.set(tuple2Enum(key), 0);
             });
-
             while (!matchTargets(bingo, targets)) {
                 let result = baseWish.wish();
                 bingo.set(tuple2Enum(result), (bingo.get(tuple2Enum(result)) ? bingo.get(tuple2Enum(result)) as number : 0) + 1);
@@ -50,14 +40,19 @@ export function MustGetChart(props: MustGetChartProps) {
             arr[total]++;
         }
     }
-    let xAxis = [];
-    let yAxis = [];
+    let xAxis: number[] = [];
+    let xxAxis: number[] = [];  // key: xAxis的value; value: xAxis的index
+    let yAxis: number[] = [];
+    let yyAxis: number[] = [];  // yAxis的累计
     for (let i = 0; i < arr.length; i++) {
         if (arr[i]) {
             xAxis.push(i);
+            xxAxis[i] = xAxis.length - 1;
             yAxis.push(arr[i]);
+            yyAxis.push((yyAxis[yyAxis.length - 1] ?? 0) + arr[i]);
         }
     }
+    const {markLineData, visualPieces} = areaParams({xAxis, yAxis, xxAxis, yyAxis}, simulateTimes);
     return <ReactECharts
         option={{
             xAxis: {
@@ -71,24 +66,35 @@ export function MustGetChart(props: MustGetChartProps) {
                 {
                     data: yAxis,
                     type: 'line',
+                    symbol: 'none',
+                    lineStyle: {
+                        color: '#336'
+                    },
+                    areaStyle: {},
+                    markLine: {
+                        symbol: ['none', 'none'],
+                        label: {show: false},
+                        data: markLineData
+                    },
                 }
             ],
+            visualMap: {
+                type: 'piecewise',
+                show: false,
+                dimension: 0,
+                seriesIndex: 0,
+                pieces: visualPieces
+            },
             tooltip: {
                 trigger: 'axis',
                 formatter: (params: any) => {
-                    let all = 0;
-                    for (let i = 0; i <= Number(params[0].name); i++) {
-                        if (arr[i]) {
-                            all += arr[i];
-                        }
-                    }
                     return `<div>
-                            <div>抽了${params[0].name}次</div>
-                            <div>模拟了${params[0].value}次</div>
-                            <div>当前概率: ${params[0].value / simulateTimes * 100}%</div>
-                            <div>左累计概率: ${all / simulateTimes * 100}%</div>
-                            <div>右累计概率: ${(simulateTimes - all + params[0].value) / simulateTimes * 100}%</div>
-                        </div>`
+                                <div>抽了${params[0].name}次</div>
+                                <div>模拟了${params[0].value}次</div>
+                                <div>当前概率: ${params[0].value / simulateTimes * 100}%</div>
+                                <div>左累计概率: ${yyAxis[xxAxis[Number(params[0].name)]] / simulateTimes * 100}%</div>
+                                <div>右累计概率: ${(simulateTimes - yyAxis[xxAxis[Number(params[0].name)]] + params[0].value) / simulateTimes * 100}%</div>
+                            </div>`
                 }
             }
         }}
@@ -105,4 +111,42 @@ export class MustGetWish {
     targets!: Map<[number, string], number>;
     current?: number[];
     state?: number[][];
+}
+
+function matchTargets(bingo: Map<[number, string], number>, targets: Map<[number, string], number>) {
+    let flag: boolean = true;
+    targets.forEach((value, key) => {
+        if ((bingo.get(key) as number) < value) {
+            flag = false;
+        }
+    });
+    return flag;
+}
+
+function areaParams(props: { xAxis: number[], yAxis: number[], xxAxis: number[], yyAxis: number[] }, simulateTimes: number) {
+    let areaLine: number[] = [];
+    const {xAxis, yAxis, xxAxis, yyAxis} = props;
+    for (let i = 0; i < xAxis.length; i++) {
+        if (!areaLine[1] && yyAxis[i] >= simulateTimes / 4) {
+            areaLine[1] = xAxis[i];
+        } else if (!areaLine[2] && yyAxis[i] >= simulateTimes / 2) {
+            areaLine[2] = xAxis[i];
+        } else if (!areaLine[3] && yyAxis[i] >= simulateTimes * 3 / 4) {
+            areaLine[3] = xAxis[i];
+        }
+    }
+    areaLine[0] = xAxis[0];
+    areaLine[4] = xAxis[xAxis.length - 1];
+    let markLineData = [], visualPieces = []
+    for (let i = 0; i < areaLine.length - 1; i++) {
+        if (i !== 0) {
+            markLineData.push({xAxis: xxAxis[areaLine[i]]})
+        }
+        visualPieces.push({
+            ge: xxAxis[areaLine[i]],
+            lt: xxAxis[areaLine[i + 1]],
+            color: 'rgba(0,0,180,' + (0.5 / areaLine.length * (i + 1)) + ')',
+        });
+    }
+    return {markLineData, visualPieces}
 }
