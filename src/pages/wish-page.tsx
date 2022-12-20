0d925170f0ae743a05d40c02c4abc157e49fc331
lxp <hello_xp@outlook.com>
2022-12-20 14:30:28 +0800

    show page

--- src/pages/wish-page.tsx
+++ src/pages/wish-page.tsx
@@ -1,4 +1,4 @@
-import {Button, InputNumber, Layout, Select, Space, Spin} from "antd";
+import {Button, InputNumber, Layout, Select, Slider, Space, Spin} from "antd";
 import {MustGetChart} from "../charts/must-get-chart";
 import React from "react";
 import {Weapon} from "../base/weapon";
@@ -23,70 +23,82 @@ export function WishPage() {
     return (
         <div>
             <Layout>
-                <Space>
+                <Space wrap={true}>
                     <Select defaultValue={characterTargets.get(tuple2Enum([5, 'up'])) ?? 0}
+                            style={{width: '90px'}}
                             onChange={(value: number) => {
                                 characterTargets.set(tuple2Enum([5, 'up']), value);
                                 setCount(count => count + 1);
                             }}>
-                        {[0, 1, 2, 3, 4, 5, 6, 7].map(c => <Select.Option key={c}
-                                                                          value={c}>{c - 1}命5星</Select.Option>)}
+                        {[0, 1, 2, 3, 4, 5, 6, 7].map(c => <Select.Option key={c} value={c}>
+                            {c === 0 ? '无' : `${c - 1}命5星`}</Select.Option>)}
                     </Select>
                     <Select defaultValue={weaponTargets.get(tuple2Enum([5, 'up'])) ?? 0}
+                            style={{width: '90px'}}
                             onChange={(value: number) => {
                                 weaponTargets.set(tuple2Enum([5, 'up']), value);
                                 setCount(count => count + 1);
                             }}>
-                        {[0, 1, 2, 3, 4, 5].map(c => <Select.Option key={c} value={c}>{c}精5星</Select.Option>)}
+                        {[0, 1, 2, 3, 4, 5].map(c => <Select.Option key={c} value={c}>
+                            {c === 0 ? '无' : `${c}精5星`}</Select.Option>)}
                     </Select>
                     <Select defaultValue={characterTargets.get(tuple2Enum([4, 'up1'])) ?? 0}
+                            style={{width: '90px'}}
                             onChange={(value: number) => {
                                 characterTargets.set(tuple2Enum([4, 'up1']), value);
                                 setCount(count => count + 1);
                             }}>
-                        {[0, 1, 2, 3, 4, 5, 6, 7].map(c => <Select.Option key={c}
-                                                                          value={c}>{c - 1}命4星</Select.Option>)}
+                        {[0, 1, 2, 3, 4, 5, 6, 7].map(c => <Select.Option key={c} value={c}>
+                            {c === 0 ? '无' : `${c - 1}命4星`}</Select.Option>)}
                     </Select>
                     <Select defaultValue={weaponTargets.get(tuple2Enum([4, 'up1'])) ?? 0}
+                            style={{width: '90px'}}
                             onChange={(value: number) => {
                                 weaponTargets.set(tuple2Enum([4, 'up1']), value);
                                 setCount(count => count + 1);
                             }}>
-                        {[0, 1, 2, 3, 4, 5].map(c => <Select.Option key={c} value={c}>{c}精4星</Select.Option>)}
+                        {[0, 1, 2, 3, 4, 5].map(c => <Select.Option key={c} value={c}>
+                            {c === 0 ? '无' : `${c}精4星`}</Select.Option>)}
                     </Select>
-                    角色池已垫:
-                    <InputNumber defaultValue={0} onChange={(value: number | null) => {
-                        if (value) {
-                            setCharacterCurrent([value, 0]);
-                            setCount(count => count + 1);
-                        } else {
-                            setCharacterCurrent([0, 0]);
-                            setCount(count => count + 1);
-                        }
-                    }}></InputNumber>
+                    <InputNumber defaultValue={0} addonBefore={'角色池已垫'}
+                                 value={characterCurrent[0]}
+                                 style={{width: '165px'}}
+                                 min={0} max={89} precision={0}
+                                 onChange={(value: number | null) => {
+                                     if (value) {
+                                         setCharacterCurrent([value, 0]);
+                                         setCount(count => count + 1);
+                                     } else {
+                                         setCharacterCurrent([0, 0]);
+                                         setCount(count => count + 1);
+                                     }
+                                 }}></InputNumber>
                     <Select defaultValue={characterState[0][0]} onChange={(value: number) => {
                         setCharacterState([[value], characterState[1]]);
                         setCount(count => count + 1);
                     }}>
-                        <Select.Option value={0}>小保底</Select.Option>
-                        <Select.Option value={1}>大保底</Select.Option>
+                        <Select.Option value={0}>角色池小保底</Select.Option>
+                        <Select.Option value={1}>角色池大保底</Select.Option>
                     </Select>
-                    武器池已垫:
-                    <InputNumber defaultValue={0} onChange={(value: number | null) => {
-                        if (value) {
-                            setWeaponCurrent([value, 0]);
-                            setCount(count => count + 1);
-                        } else {
-                            setWeaponCurrent([0, 0]);
-                            setCount(count => count + 1);
-                        }
-                    }}></InputNumber>
+                    <InputNumber defaultValue={0} addonBefore={'武器池已垫'}
+                                 value={characterCurrent[0]}
+                                 style={{width: '165px'}}
+                                 min={0} max={79} precision={0}
+                                 onChange={(value: number | null) => {
+                                     if (value) {
+                                         setWeaponCurrent([value, 0]);
+                                         setCount(count => count + 1);
+                                     } else {
+                                         setWeaponCurrent([0, 0]);
+                                         setCount(count => count + 1);
+                                     }
+                                 }}></InputNumber>
                     <Select defaultValue={weaponState[0][0]} onChange={(value: number) => {
                         setWeaponState([[value, weaponState[0][1]], weaponState[1]]);
                         setCount(count => count + 1);
                     }}>
-                        <Select.Option value={0}>小保底</Select.Option>
-                        <Select.Option value={1}>大保底</Select.Option>
+                        <Select.Option value={0}>武器池小保底</Select.Option>
+                        <Select.Option value={1}>武器池大保底</Select.Option>
                     </Select>
                     <Select defaultValue={weaponState[0][1]} onChange={(value: number) => {
                         weaponState[0][1] = value;
@@ -97,16 +109,12 @@ export function WishPage() {
                         <Select.Option value={1}>1定轨</Select.Option>
                         <Select.Option value={2}>满定轨</Select.Option>
                     </Select>
-                    模拟
                     <Select defaultValue={20000} onChange={(value: number) => {
                         setSimulateTimes(value)
                         setCount(count => count + 1);
                     }}>
-                        <Select.Option value={20000}>20000</Select.Option>
-                        <Select.Option value={50000}>50000</Select.Option>
-                        <Select.Option value={100000}>100000</Select.Option>
+                        {[20000,50000,100000].map(value=><Select.Option value={value}>模拟{value}次</Select.Option>)}
                     </Select>
-                    次
                     <Button onClick={() => {
                         if (!loading) {
                             setCount(count => count + 1);
