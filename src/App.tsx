ff79a6b8021ace7427acd6fac133a08e75ca66e5
lxp <hello_xp@outlook.com>
2023-04-22 17:43:33 +0800

    路由

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ function App() {
     return (<>
         <BrowserRouter>
             <Menu mode={'horizontal'}>
-                <Menu.Item><Link to={'/gacha_test/ys'}>ys</Link></Menu.Item>
-                <Menu.Item><Link to={'/gacha_test/bt'}>bt</Link></Menu.Item>
+                <Menu.Item><Link to={'./ys'}>ys</Link></Menu.Item>
+                <Menu.Item><Link to={'./bt'}>bt</Link></Menu.Item>
             </Menu>
             <AreaContext.Provider value={[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}>
-                <Routes location={'/gacha_test/ys'}>
-                    <Route path={'/gacha_test/ys'} element={<YsWishPage></YsWishPage>}/>
-                    <Route path={'/gacha_test/bt'} element={<BtWishPage></BtWishPage>}/>
+                <Routes location={'./ys'}>
+                    <Route path={'./ys'} element={<YsWishPage></YsWishPage>}/>
+                    <Route path={'./bt'} element={<BtWishPage></BtWishPage>}/>
                 </Routes>
             </AreaContext.Provider>
         </BrowserRouter>
